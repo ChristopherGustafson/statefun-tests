53b257e06ba3cfa80bad924f3f14aa278c6324ac
ChristopherGustafson <christopher.gustafson@outlook.com>
2022-02-22 14:06:46 +0100

    Fixed serialization issue at second embedded function

--- statefun-harness/src/main/java/statefun_examples/MySecondFunction.java
+++ statefun-harness/src/main/java/statefun_examples/MySecondFunction.java
@@ -4,11 +4,15 @@ import org.apache.flink.statefun.sdk.Context;
 import org.apache.flink.statefun.sdk.FunctionType;
 import org.apache.flink.statefun.sdk.StatefulFunction;
 import org.apache.flink.statefun.sdk.annotations.Persisted;
+import org.apache.flink.statefun.sdk.reqreply.generated.TypedValue;
 import org.apache.flink.statefun.sdk.state.PersistedValue;
 import org.apache.flink.statefun.sdk.Address;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import harness.protos.OutputMsg;
 import statefun_examples.MyMessages.InternalMessage;
+
+import java.nio.charset.StandardCharsets;
 
 public class MySecondFunction implements StatefulFunction {
 
@@ -16,15 +20,22 @@ public class MySecondFunction implements StatefulFunction {
     private final PersistedValue<Double> SEEN = PersistedValue.of("seen", Double.class);
 
     @Override
-    public void invoke(Context context, Object message) {
-        if(!(message instanceof OutputMsg)){
+    public void invoke(Context context, Object message) throws IllegalArgumentException {
+
+        if(!(message instanceof TypedValue)){
             throw new IllegalArgumentException("Unexpected message type: " + message);
         }
 
         double newSeen = SEEN.getOrDefault(0.0) + 1.0;
         SEEN.set(newSeen);
 
-        OutputMsg outputMsg = (OutputMsg) message;
+        TypedValue typed = (TypedValue) message;
+        OutputMsg outputMsg = null;
+        try {
+            outputMsg = OutputMsg.parseFrom(typed.toByteArray());
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
         context.send(MyConstants.MESSAGE_EGRESS, outputMsg);
 
     }
